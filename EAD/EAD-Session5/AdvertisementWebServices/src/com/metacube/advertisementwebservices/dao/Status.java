c8848990220211f57ab0512573bd807a45ea5a78
Ramkumar Kulriya <ramkumar.kulriya@metacube.com>
2018-09-06 16:20:04 +0530

    Revert "Revert "Assignment of EAD Session5""

---
+++ EAD/EAD-Session5/AdvertisementWebServices/src/com/metacube/advertisementwebservices/dao/Status.java
@@ -0,0 +1,8 @@
+package com.metacube.advertisementwebservices.dao;
+
+/**
+ * Enums for status
+ */
+public enum Status {
+    INSERTED, UPDATED, DELETED, DUPLICATE, NOT_FOUND, INVALID, ERROR, NOT_INSERTED, NOT_UPDATED, NOT_DELETED;
+}
